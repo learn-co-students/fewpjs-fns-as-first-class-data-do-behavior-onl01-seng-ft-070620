f04adc23d0d50c7b473f96af0ad6b66292246b0e
Jake <jrdriscoll17@gmail.com>
2020-10-07 14:14:00 -0500

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,20 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  const splitTime = time.split(':')
+  const formattedTime = parseInt(splitTime[0])
+  if (formattedTime < 12) {
+    return "Good Morning"
+  } else if (formattedTime > 12 && formattedTime < 19) {
+    return "Good Afternoon"
+  } else
+    return "Good Evening"
+}
+
+function displayMessage(message) {
+  const greeting = document.getElementById('greeting')
+  greeting.innerHTML = greet(message)
+}
+

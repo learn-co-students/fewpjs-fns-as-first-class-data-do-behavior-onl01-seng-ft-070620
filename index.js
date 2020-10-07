31a22a4ad58f77f39c26f6c1df210acbb7a7ff07
Ethan <eleetyson@gmail.com>
2020-10-07 10:07:50 -0400

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,27 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
+  const timeString = document.getElementById('time').values
   displayMessage(greet(timeString))
 }
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+// returns a greeting based on the given argument, a time
+function greet(time) {
+  let timeStr = time.split(":").join("");
+  let parsed = parseInt(timeStr);
+  if (parsed < 1200) {
+    return "Good Morning";
+  } else if (parsed < 1700) {
+    return "Good Afternoon";
+  } else {
+    return "Good Evening";
+  };
+}
+
+// updates the text inside the #greeting nodes
+function displayMessage(newText) {
+  let element = document.getElementById("greeting");
+  element.innerText = `${newText}`;
+}
